1d3ff5bccb483321be95ca7b9b5fc49d96d36137
PaulKulm <132549035+PaulKulm@users.noreply.github.com>
2023-05-04 10:31:03 +0200

    Add files via upload

---
+++ INDIT_Labor_1.py
@@ -0,0 +1,12 @@
+a="Hello,"
+b="World!"
+print(a+b)
+
+x=int(input("Zahl 1:"))
+y=int(input("Zahl 2:"))
+print("Ergebnis der Adittion:",x+y)
+print("Ergebnis der Subtraktion:",x-y)
+print("Ergebnis der Multiplikation:",x*y)
+print("Ergebnis der Division:",x/y)
+
+
